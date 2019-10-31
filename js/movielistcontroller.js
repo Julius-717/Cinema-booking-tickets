3f10f912dd8d4fe0f576b09945cc3d6a8f3df7d9
Julius-717 <waynejulius10@gmail.com>
2019-11-01 01:11:25 +0300

    add movielist to movielistcontroller.js

--- js/movielistcontroller.js
+++ js/movielistcontroller.js
@@ -6,19 +6,19 @@ app.controller('moviesCtrl',['$scope', function($scope,filterFilter,$http) {
     $scope.recordDetails= undefined;
     $scope.languageslist = ["English"];
     $scope.movieslist= [
-        {"movieId": "0001", "moviename" : "Aval", "imageurl" : "http://bit.ly/2hZVKQX", "movielanguage":"Tamil" },
-        {"movieId": "0002", "moviename" : "Blade Runner", "imageurl" : "http://bit.ly/2zLHVQA", "movielanguage":"English" },
-        {"movieId": "0003", "moviename" : "Geostorm", "imageurl" : "http://bit.ly/2yN2Q5K", "movielanguage":"English" },
-        {"movieId": "0004", "moviename" : "Golmaal Again", "imageurl" : "http://bit.ly/2zwDQNg", "movielanguage":"Hindi" },
-        {"movieId": "0005", "moviename" : "ITTEFAQ", "imageurl" : "http://bit.ly/2jeP9Wg", "movielanguage":"Hindi" },
-        {"movieId": "0006", "moviename" : "Mersal", "imageurl" : "http://bit.ly/2zxOqFA", "movielanguage":"tamil" },
-        {"movieId": "0011", "moviename" : "PSV Garuda Vega", "imageurl" : "http://bit.ly/2iJlUqH", "movielanguage":"telugu" },
-        {"movieId": "0011", "moviename" : "Raja The Great", "imageurl" : "http://bit.ly/2yN6Z9Y", "movielanguage":"telugu" },
-        {"movieId": "0007", "moviename" : "Secret Superstar", "imageurl" : "http://bit.ly/2jdUqgS", "movielanguage":"hindi" },
-        {"movieId": "0008", "moviename" : "Solo", "imageurl" : "http://bit.ly/2ymUbTa", "movielanguage":"malayalam" },
-        {"movieId": "0009", "moviename" : "The Foreigner", "imageurl" : "http://bit.ly/2jdVbGH", "movielanguage":"english" },
-        {"movieId": "0010", "moviename" : "Thor Ragnarok", "imageurl" : "http://bit.ly/2zrFGBx", "movielanguage":"english" },
-        {"movieId": "0011", "moviename" : "Villain", "imageurl" : "http://bit.ly/2zwRaBb", "movielanguage":"malayalam" }
+        {"movieId": "0001", "moviename" : "Titanic", "imageurl" : "https://upload.wikimedia.org/wikipedia/en/1/19/Titanic_%28Official_Film_Poster%29.png", "movielanguage":"English" },
+        {"movieId": "0002", "moviename" : "Sonic", "imageurl" : "https://i.ytimg.com/vi/4mW9FE5ILJs/maxresdefault.jpg", "movielanguage":"English" },
+        {"movieId": "0003", "moviename" : "Maleficent", "imageurl" : "https://upload.wikimedia.org/wikipedia/en/7/7b/Maleficent_Mistress_of_Evil_%28Official_Film_Poster%29.png", "movielanguage":"English" },
+        {"movieId": "0004", "moviename" : "Joker", "imageurl" : "https://i.ytimg.com/vi/X4Zwpl7VbhA/maxresdefault.jpg", "movielanguage":"English" },
+        {"movieId": "0005", "moviename" : "Hellboy", "imageurl" : "https://upload.wikimedia.org/wikipedia/en/thumb/f/fc/Hellboy_%282019%29_theatrical_poster.png/220px-Hellboy_%282019%29_theatrical_poster.png", "movielanguage":"English" },
+        {"movieId": "0006", "moviename" : "Dark phoenix", "imageurl" : "https://m.media-amazon.com/images/M/MV5BMjAwNDgxNTI0M15BMl5BanBnXkFtZTgwNTY4MDI1NzM@._V1_.jpg", "movielanguage":"English" },
+        {"movieId": "0011", "moviename" : "Endgame", "imageurl" : "https://images-na.ssl-images-amazon.com/images/I/81gCaSnFTvL._SL1364_.jpg", "movielanguage":"English" },
+        {"movieId": "0011", "moviename" : "Aladdin", "imageurl" : "https://upload.wikimedia.org/wikipedia/en/9/9a/Aladdin_%28Official_2019_Film_Poster%29.png", "movielanguage":"English" },
+        {"movieId": "0007", "moviename" : "Charlie`s angels", "imageurl" : "https://upload.wikimedia.org/wikipedia/en/2/2a/Charlie%27s_Angels_%28Official_2019_Film_Poster%29.png", "movielanguage":"English" },
+        {"movieId": "0008", "moviename" : "Birds of prey", "imageurl" : "https://upload.wikimedia.org/wikipedia/en/b/b4/BirdsofPreyHarley.jpg", "movielanguage":"English" },
+        {"movieId": "0009", "moviename" : "Gemini man", "imageurl" : "https://i.ytimg.com/vi/6orc_lHvJKY/maxresdefault.jpg", "movielanguage":"English" },
+        {"movieId": "0010", "moviename" : "Hobbs & Shaw", "imageurl" : "https://upload.wikimedia.org/wikipedia/en/thumb/0/00/Fast_%26_Furious_Presents_Hobbs_%26_Shaw_-_theatrical_poster.jpg/220px-Fast_%26_Furious_Presents_Hobbs_%26_Shaw_-_theatrical_poster.jpg", "movielanguage":"english" },
+        {"movieId": "0011", "moviename" : "Angel has fallen", "imageurl" : "https://upload.wikimedia.org/wikipedia/en/thumb/1/13/Angel_Has_Fallen_poster.jpg/220px-Angel_Has_Fallen_poster.jpg", "movielanguage":"English" }
     ]
 
     $scope.showDetails = function (record) {
